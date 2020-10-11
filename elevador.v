572f7f0c10bfa9a6c155ef9ca551460a16e66096
Angardo18 <48691615+Angardo18@users.noreply.github.com>
2020-10-11 02:09:47 -0600

    Testbench completo

--- elevador.v
+++ elevador.v
@@ -145,7 +145,234 @@ module FsmElevador(input wire emergencia, igual, menor, mayor,clk, rst ,output r
 
 endmodule
 
-module FsmPuerta(input wire llego, emergencia, necesario,hecho, persona,abrir, piso,output [1:0] estado, output y );
+module FsmPuerta(input wire llego, emergencia, necesario,hecho, persona,abrir, piso,clk,rst,output reg [1:0] estado, output wire y );
 
+    wire [1:0] nextEstado;
+    //sn1 = s0 H  + s1 Ll A' Pe' Pi' ;
+    assign nextEstado[1] = (estado[0] & hecho) | (estado[1] & llego  & ~abrir & ~persona & ~piso );
+    //sn0 = s0 H'  + s1 A  + s1 Pe  + s1 Pi  + s1' s0' Ll E' N;
+    assign nextEstado[0] = (estado[0] & ~hecho) | (estado[1] & piso) | (estado[1] & abrir) | (estado[1] & persona) | (~estado[1] & ~estado[0] & llego & necesario & ~emergencia );
+    assign y =  estado[0] ;
+    always @ (posedge clk, posedge rst) begin
+        estado <= (rst? 2'b10 : nextEstado);
+
+    end 
+
+
+endmodule
+
+module OneHotToBinary(input wire [3:0]oneHot , output reg [1:0] binary);
+    // segun el diseño del sistema, el resto de opciones nunca han de ser posibles, en funcionamiento normal.
+    always @(oneHot) begin
+        case(oneHot)
+            4'b0001: binary <= 2'b00;
+            4'b0010: binary <= 2'b01;
+            4'b0100: binary <= 2'b10;
+            4'b1000: binary <= 2'b11;
+        endcase
+    end
+
+endmodule
+
+module OneHotTo7S(input wire [3:0] oneHot, output reg [6:0] segmentos );
+
+    always @(oneHot) begin
+    // el resto de combinaciones nunca deberian de suceder
+        case(oneHot)
+            4'b0001: segmentos <= 7'b0000110;
+            4'b0010: segmentos <= 7'b1011011;
+            4'b0100: segmentos <= 7'b1001111;
+            4'b1000: segmentos <= 7'b1100110;
+        endcase
+    end
+
+endmodule
+
+module LookNextFloor(input wire [2:0] estado, input wire accion, output wire mantener, output wire [1:0] y );
+    /*este modulo es el encargado de que dependiendo del estado en el que se encuentre la FSM piso, y la accion que 
+      esta realizando me de el piso al que esta por llegar, la salida va a un mux de 4 entradas, con lo que se selecciona
+      la señal que corresponde la entrada de selección de piso y peticion, para ver si es necesario hacer una parada 
+      en el siguiente piso la salida de mantener es para indicarle a un latch si deja pasar la señal que se le esta proporcionando
+      o si mantiene el valor anterior*/
+
+    //y1 = E1 E0  + E0' A;
+    assign y[1] = (estado[1] & estado[0]) | (~estado[0] & accion);
+    //y0 = E0 A + E0' A';
+    assign y[0] = (estado[0] & accion) | (~estado[0] & ~accion);
+    //M = E2 E1  + E2 E0 ;
+    assign mantener = (estado[2] & estado[1]) | (estado[2] & estado[0]);
+
+endmodule
+
+module Mux4(input wire [1:0] s, input wire d,c,b,a, output wire y );
+    assign y = (s[1] ? ( s[0] ? d : c ) : (s[0] ? b : a)  );
+endmodule
+
+module  FlipFlopD(input wire D, clk, preset, rst, output reg Q);
+    // flip flop tipo D 
+    always @ (posedge clk,posedge rst) Q <= (rst ? preset : D ); 
+endmodule
+
+module FlipFlopJK(input j,k,clk,rst, output reg Q);
+    //modelo de un flip flop jk
+    always @ (posedge clk, posedge rst) begin
+        if (rst) Q <= 1'b0;
+        else begin
+            case( {j,k})
+                2'b00: Q<= Q;
+                2'b01: Q<= 1'b0;
+                2'b10: Q<= 1'b1;
+                2'b11: Q<= ~Q;
+            endcase
+        end
+    end
+endmodule
+
+module Contador(input clk,rst, output reg [5:0] q);
+    //contador de 6 bits
+    always @(posedge clk, posedge rst) q<= (rst? 4'b0000 : q+1 );
+endmodule
+
+
+module SistemaElevador(input wire [7:0] piso,
+                       input wire [3:0] peticion,
+                       input wire [3:0] seleccion,
+                       input wire abrir,emergencia,persona,puerta,clk,rst,
+                       output wire [6:0] display,
+                       output wire [1:0] accion,
+                       output wire direccion,accionPuerta);
+    
+    /* este es el modulo principal en donde se arma todo el sistema del elevador, tal y como fue 
+        hecho en circuitverse, las entradas tienen los siguientes significados:
+        Piso: Son sensores los cuales detectan el cual piso se encuentra el elevador, siendo que 00000011 la señal
+             enviada por los sensores indica que se encuentra en el piso 1, si es 00000110 indica que se encuentra entre
+             el piso 1 y el 2, y de igual forma con los demas bits
+        peticion: son los botones dentro del elevador en donde las personas seleccionan el piso al que quieren ir, si el bit
+                  n-1 esta encendido quiere decir que se desea ir a piso n.
+        seleccion: la misma funcion que la entrada de peticion con la diferencia que estos son los botones que se encuentran
+                en el exterior del elevador para que las pesonas indiquen que desean usar el elevador, el si el bit n-1 esta 
+                encendido significa que hay una persona en el piso n que quiere usar el elevador.
+        abrir: boton dentro del elevador para la apertura manual del elevador
+        emergencia: boton dentro del elevador para activar los frenos de forma manual
+        persona: sensor que detecta si una persona ha pasado por la puerta del elevador para reiniciar el contador
+        puerta: sensor que indica si la puerta esta totalmente abierta (1) o totalmente cerrada (0) se obvia los casos en donde
+            se encuentre cerrando o abriendo la puerta, se asume que en estos casos la señal mantiene su valor anterior.
+        
+        las salidas tienen el siguiente significado:
+
+        Display: son los valores que se tienen que colocar en el 7 segmentos para visualizar el piso en el que se encuentra,
+                siendo el bit mas significativo el que representa g y el menos significativo representa la terminal a.
+        accion: indica que accion tiene que realizar el elevador 00 freno, 01 subir 10 bajar.
+        direccion: indica en que direccion se esta moviendo el elevador para referencia de los usuarios 1 subiendo 0 bajando
+        accionPuerta: indica que debe hacer la puerta, 1 abrir y 0 cerrar, no indica si esta abierta o cerrada, sino si la puerta
+                    debe abrirse o cerrarse.*/
+    
+    //----- compresion de imformacion del piso--------------------------------------------------
+    wire [3:0] pisoComprimido;
+
+    assign pisoComprimido[0] = piso[0] & piso[1];
+    assign pisoComprimido[1] = piso[2] & piso[3];
+    assign pisoComprimido[2] = piso[4] & piso[5];
+    assign pisoComprimido[3] = piso[6] & piso[7];
+    //------------------- memoria con FF jk para los botones de peticion y seleccion----------------------------------
+    //peticion, todas estas "memorias" se reinician cuando el elevador se encuentra en el piso correspondiente, y la puerta
+    //se abre.
+    wire [3:0] memPeticion;
+    FlipFlopJK  ffJK1(peticion[0],1'b0,clk,(pisoComprimido[0] & accionPuerta) | rst, memPeticion[0] );
+    FlipFlopJK  ffJK2(peticion[1],1'b0,clk,(pisoComprimido[1] & accionPuerta) | rst, memPeticion[1] );
+    FlipFlopJK  ffJK3(peticion[2],1'b0,clk,(pisoComprimido[2] & accionPuerta) | rst, memPeticion[2] );
+    FlipFlopJK  ffJK4(peticion[3],1'b0,clk,(pisoComprimido[3] & accionPuerta) | rst, memPeticion[3] );
+    //seleccion
+    wire [3:0] memSeleccion;
+    FlipFlopJK  ffJK5(seleccion[0],1'b0,clk,(pisoComprimido[0] & accionPuerta) | rst, memSeleccion[0] );
+    FlipFlopJK  ffJK6(seleccion[1],1'b0,clk,(pisoComprimido[1] & accionPuerta) | rst, memSeleccion[1] );
+    FlipFlopJK  ffJK7(seleccion[2],1'b0,clk,(pisoComprimido[2] & accionPuerta) | rst, memSeleccion[2] );
+    FlipFlopJK  ffJK8(seleccion[3],1'b0,clk,(pisoComprimido[3] & accionPuerta) | rst, memSeleccion[3] );
+    // uso de la primera FSM piso
+    wire accionAnterior; // indica si anteriormente el elevador estaba subiendo o bajando
+    wire openDoor;// indica si la puerta esta 
+    wire llego; //indica si llego al piso que necesita. esta es la salida igual del comparador
+    wire [3:0] floor; //or de memSeleccion y memPeticion bit a bit
+    wire [2:0] estadoFSMpiso; //estado de la FSM piso
+    wire [3:0] nextFloor;//piso siguente al cual debe ir el elevador
+    //------------ operaciones para establecer las entradas de la FSM ----------------------------------------
+    assign floor = memPeticion | memSeleccion; 
+    assign openDoor = puerta | accionPuerta; 
+    // se crea la FSM que maneja los pisos en el que se encuentra 
+    FSMpiso decidirPiso(floor,openDoor, direccion, llego, clk,rst,estadoFSMpiso,nextFloor);   
+    //valores de piso guardados en memorias con FF tipo D
+    wire [3:0] actualFloorFix;
+    wire [3:0] nextFloorFix;
+    // nodos para permitir el paso de la señal de los FF
+    wire senialPisoActual;
+    wire senialPisoSiguiente;
+
+    assign senialPisoActual = pisoComprimido[0] | pisoComprimido[1] | pisoComprimido[2] |pisoComprimido[3] ; 
+    assign senialPisoSiguiente = nextFloor[0] | nextFloor[1] |nextFloor[2] |nextFloor[3] ;
+    //se guardan los datos mientras todos los bits sean 0 si almenos 1 es diferente de 0 entonces se actualiza el valor 
+    FlipFlopD   FFD1(pisoComprimido[0], clk & senialPisoActual, 1'b1,rst,actualFloorFix[0]);
+    FlipFlopD   FFD2(pisoComprimido[1], clk & senialPisoActual,  1'b0,rst,actualFloorFix[1]);
+    FlipFlopD   FFD3(pisoComprimido[2], clk & senialPisoActual,  1'b0,rst,actualFloorFix[2]);
+    FlipFlopD   FFD4(pisoComprimido[3], clk & senialPisoActual,  1'b0,rst,actualFloorFix[3]);
+
+    FlipFlopD   FFD5(nextFloor[0], clk & senialPisoSiguiente, 1'b1, rst , nextFloorFix[0]);
+    FlipFlopD   FFD6(nextFloor[1], clk & senialPisoSiguiente,  1'b0, rst , nextFloorFix[1]);
+    FlipFlopD   FFD7(nextFloor[2], clk & senialPisoSiguiente,  1'b0, rst , nextFloorFix[2]);
+    FlipFlopD   FFD8(nextFloor[3], clk & senialPisoSiguiente,  1'b0, rst , nextFloorFix[3]);
+    //salida del 7segmentos
+
+    OneHotTo7S  oneTo7s(actualFloorFix,display);
+    
+    //comparador
+    wire menor;// siguiente < actual
+    wire mayor; //  sigiente > actual
+    Comparador  comp(actualFloorFix,nextFloorFix,llego,menor, mayor);
+    
+    wire[1:0] estadoFSMelevador;
+    //Uso y creacion de la FSM que controla la accion del elevador
+    FsmElevador fsmElevador(emergencia,llego,menor,mayor,clk,rst, estadoFSMelevador, accion);
+    // valor de la salida direccion
+    wire actualizarDireccion;
+
+    assign  actualizarDireccion = accion[1] | accion[0];
+
+    //memoria del estado se actualiza solo si actualizarDireccion es 1
+    FlipFlopD FFD9(accion[0],clkSlow & actualizarDireccion ,1'b0,rst,direccion);
+    
+    //---------- Señales internas necesarias para el funcionamiento de la FSM puerta ----------------------
+    wire [1:0] controlMux1; //control del mux que manda la señal necesario a la FSM puerta
+    wire mantener; ///senial para mantener o no el dato salido del mux
+    wire necesarioPrev;
+    wire necesario; //va conectada a la entrada necesario de la FSM puerta
+
+    LookNextFloor   look(estadoFSMpiso,direccion,mantener, controlMux1);
+
+    Mux4 mux1(controlMux1,floor[3],floor[2],floor[1],floor[0],necesarioPrev);
+    // senial necesario de la fsm piso
+    FlipFlopD   FFD10(necesarioPrev,clk & mantener,1'b0 , rst , necesario);
+
+    // senial piso de la FSM piso
+    wire [1:0] controlMux2;
+    wire senialPiso;
+    OneHotToBinary  oneToBinary(actualFloorFix,controlMux2);
+    Mux4    mux2(controlMux2,peticion[3],peticion[2],peticion[1],peticion[0],senialPiso);
+
+    //temporizador para la puerta
+    wire [5:0] temporizador;
+    wire hecho; //senial hecho de la fsm puerta
+    wire resetTem; //senial que reinicia el temporizador
+    //senial de renicio para el temporizador
+    assign  resetTem = ~puerta | abrir | persona | senialPiso | (temporizador[2] & temporizador[1]  ) | rst; //
+    // creacion del contador
+    Contador cont(clk,resetTem,temporizador);
+
+
+    assign hecho = temporizador[2] & temporizador[0]; // cuenta hasta 10, para no hacer tan largo el diagrama de timming 
+
+    wire [1:0] estadoFsmPuerta;
+    // creacion de la FSM puerta
+    FsmPuerta fsmPuerta(llego, emergencia, necesario,hecho,persona,abrir,senialPiso,clk,rst, estadoFsmPuerta,accionPuerta);
+
+    
 
 endmodule
--- elevador_tb.v
+++ elevador_tb.v
@@ -1,38 +1,71 @@
-
 module tb();
-    reg[3:0] piso;
-    reg puerta, accion, llego, clk, rst;
-    wire [2:0] estado;
-    wire [3:0] siguiente;
+    reg  [7:0] piso;
+    reg  [3:0] peticion;
+    reg  [3:0] seleccion;
+    reg  abrir,emergencia,persona,puerta,clk,rst;
+    wire [6:0] display;
+    wire [1:0] accion;
+    wire direccion,accionPuerta;
+ 
+    SistemaElevador sistema(piso,peticion, seleccion,abrir,emergencia,persona,puerta,clk,rst,
+                            display,accion,direccion,accionPuerta);
+    
 
-    FSMpiso fsm(piso,puerta,accion, llego, clk, rst,estado,siguiente);
 
-    initial clk = 0;
+
+    initial clk = 1;
 
     initial begin
-        #1 rst= 0; accion = 0; llego = 0; puerta = 0; piso = 4'b000;
-        #1 rst =1; 
-        #1 rst = 0;
-        #4 puerta = 1;
-        #4 piso = 4'b0100;
-        #4 puerta=0 ;piso = 4'b1100;accion = 1;
-        #4 piso = 4'b0110;
-        #4 llego = 1;
-        #4 puerta = 1; piso=4'b0110; 
-        #4 puerta = 0;
-        #4 piso = 4'b0100; llego =0;
-        #8 llego = 0;
-        #4 llego =1; 
-        #4 puerta = 1;
-        #4 piso = 4'b1000;
-        #4 puerta = 0; llego = 0;
-        #8 llego = 1;
+	piso=	8'b00000011;	peticion =	4'b0000;	seleccion = 	4'b0000 ;	abrir = 	0;	emergencia = 	0;	persona =	0;	puerta = 	0;	rst =	0;	
+#1															rst =	1;	
+#1															rst =	0;	
+#2			peticion =	4'b0001;													
+#2			peticion =	4'b0000;													
+#4													puerta= 	1;			//luego de que se envia la senial de abrir se abre la puerta
+#14													puerta= 	0;			
+#10			peticion =	4'b1000;	seleccion = 	4'b0110;											
+#4	piso =	8'b00000110;	peticion =	4'b0000;	seleccion = 	4'b0000 ;											
+#4									emergencia = 	1;	//se activan los frenos de emergencia						
+#8									emergencia = 	0;	// se desactivan						
+#4	piso =	8'b00001100;	// llego al piso 2														
+#4													puerta =	1;			
+#4							abrir = 	1;	 // alguen presiono el boton de abrir								
+#2							abrir = 	0;			persona = 	1;	//paso una persona 				
+#2											persona = 	0;					
+#14													puerta =	0;			
+#10	piso =	8'b00011000;															
+#8	piso = 	8'b00110000;	//llego al piso 3														
+#4													puerta = 	1;			
+#2											persona = 	1;					
+#6											persona= 	0;					
+#14													puerta = 	0;			
+#16	piso = 	8'b01100000;															
+#10	piso = 	8'b11000000;															
+#4													puerta = 	1;			
+#4							abrir = 	1;	 // alguen presiono el boton de abrir								
+#2							abrir = 	0;			persona = 	1;	//paso una persona 				
+#2											persona = 	0;					
+#14													puerta = 	0;			
+#4			peticion =	4'b1000;													
+#4			peticion =	4'b0000	;puerta = 1;// alguien en el piso 4 quiere entrar 								puerta =	1;			
+#4					seleccion =	4'b0001;	// pide ir al piso 1										
+#6					seleccion =	4'b0000	;										
+#8													puerta =	0;			
+#10	piso = 	8'b01100000;															
+#10	piso = 	8'b00110000;															
+#10	piso =	8'b00011000;															
+#10	piso =	8'b00001100;															
+#10	piso =	8'b00000110;															
+#10	piso = 	8'b00000011;															
+#4													puerta =	1;			
+#14													puerta =	0;			
+			
 
-        #5 $finish;
+       #10 $finish;
     end
 
 
-    always #2 clk = ~clk;
+    always #1 clk = ~clk;
 
     initial begin
         $dumpfile("elevador_tb.vcd");
